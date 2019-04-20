205f76a8e8239c47f9b27e0d535c519038b23c7c
Steve Yen <steve.yen@gmail.com>
2019-04-20 14:48:48 -0700

    optimize MakeProjectFunc with emitCaptured

--- project.go
+++ project.go
@@ -7,36 +7,33 @@ import (
 func MakeProjectFunc(fields base.Fields, types base.Types,
 	projections []interface{}, outTypes base.Types) (
 	lazyProjectFunc base.ProjectFunc) {
-	var lazyExprFuncs []base.ExprFunc
-
-	for _, projection := range projections {
-		expr := projection.([]interface{})
-
-		outTypes = append(outTypes, "")
-
-		if LazyScope {
-			var lazyExprFunc base.ExprFunc
-
-			lazyExprFunc =
-				MakeExprFunc(fields, types, expr, outTypes, "", "") // <== notLazy
-
-			lazyExprFuncs = append(lazyExprFuncs, lazyExprFunc)
-		}
+	for range projections {
+		outTypes = append(outTypes, "") // TODO: projected out type.
 	}
 
 	lazyProjectFunc = func(lazyVals, lazyValsPre base.Vals) (
 		lazyValsOut base.Vals) {
 		lazyValsOut = lazyValsPre // Optional pre-alloc'ed slice.
 
-		for _, lazyExprFunc := range lazyExprFuncs {
-			var lazyValProjected base.Val
+		for _, projection := range projections { // <== notLazy
+			expr := projection.([]interface{}) // <== notLazy
 
-			lazyValProjected = lazyExprFunc(lazyVals)
+			if LazyScope {
+				var lazyExprFunc base.ExprFunc
+				_ = lazyExprFunc
 
-			// NOTE: The lazyVals is stable while we are building up
-			// the lazyValsOut, so no need to deep copy lazyVal yet.
-			lazyValsOut = append(lazyValsOut, lazyValProjected)
-		}
+				lazyExprFunc =
+					MakeExprFunc(fields, types, expr, outTypes, "project", "PF") // <== notLazy
+
+				var lazyVal base.Val
+
+				lazyVal = lazyExprFunc(lazyVals) // <== emitCaptured: "project" PF
+
+				// NOTE: lazyVals are stable while we are building up
+				// lazyValsOut, so no need to deep copy lazyVal yet.
+				lazyValsOut = append(lazyValsOut, lazyVal)
+			}
+		} // <== notLazy
 
 		return lazyValsOut
 	}
