9e296f3d4b3d291d2ec6e3ace78efb4d58b51c3f
Steve Yen <steve.yen@gmail.com>
2019-04-27 16:00:59 -0700

    test order-by DESC

--- test/cases_simple.go
+++ test/cases_simple.go
@@ -1298,4 +1298,64 @@ var TestCasesSimple = []TestCaseSimple{
 			base.Vals{[]byte("12"), []byte("22")},
 		},
 	},
+	{
+		about: "test csv-data scan->order-by 1 record",
+		o: base.Op{
+			Kind:   "order-by-offset-limit",
+			Fields: base.Fields{"a", "b"},
+			Params: []interface{}{
+				[]interface{}{
+					[]interface{}{"field", "a"},
+				},
+				[]interface{}{
+					"asc",
+				},
+			},
+			ParentA: &base.Op{
+				Kind:   "scan",
+				Fields: base.Fields{"a", "b"},
+				Params: []interface{}{
+					"csvData",
+					`
+10,20
+`,
+				},
+			},
+		},
+		expectYields: []base.Vals{
+			base.Vals{[]byte("10"), []byte("20")},
+		},
+	},
+	{
+		about: "test csv-data scan->order-by DESC",
+		o: base.Op{
+			Kind:   "order-by-offset-limit",
+			Fields: base.Fields{"a", "b"},
+			Params: []interface{}{
+				[]interface{}{
+					[]interface{}{"field", "b"},
+				},
+				[]interface{}{
+					"desc",
+				},
+			},
+			ParentA: &base.Op{
+				Kind:   "scan",
+				Fields: base.Fields{"a", "b"},
+				Params: []interface{}{
+					"csvData",
+					`
+10,20
+11,21
+12,22
+`,
+				},
+			},
+		},
+		expectYields: []base.Vals{
+			base.Vals{[]byte("12"), []byte("22")},
+			base.Vals{[]byte("11"), []byte("21")},
+			base.Vals{[]byte("10"), []byte("20")},
+		},
+	},
 }
