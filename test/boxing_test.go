21409598f015e942a00712b318d85ed0e96a61fd
Steve Yen <steve.yen@gmail.com>
2019-05-05 20:31:12 -0700

    boxing conclusions

--- test/boxing_test.go
+++ test/boxing_test.go
@@ -5,6 +5,12 @@ import (
 )
 
 // Test via: go test -v -bench=Boxing -benchmem ./test
+//
+// BenchmarkBoxing-8 50000000 35.6 ns/op 32 B/op 1 allocs/op
+//
+// Conclusion is that some non-scalar types are boxed, which leads to
+// some memory allocations.
+//
 func BenchmarkBoxing(b *testing.B) {
 	m := []interface{}{123.3}
 
