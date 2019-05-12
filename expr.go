adf550a61bc073e6a5d3bc62e218c910dddb29e4
Steve Yen <steve.yen@gmail.com>
2019-05-11 21:48:42 -0700

    ExprValsCanonical

--- expr.go
+++ expr.go
@@ -6,8 +6,9 @@ import (
 
 // ExprCatalog is the default registry of known expression functions.
 var ExprCatalog = map[string]base.ExprCatalogFunc{
-	"json":      ExprJson,
-	"labelPath": ExprLabelPath,
+	"json":          ExprJson,
+	"labelPath":     ExprLabelPath,
+	"valsCanonical": ExprValsCanonical,
 }
 
 // -----------------------------------------------------
@@ -69,6 +70,54 @@ func ExprLabelPath(lzVars *base.Vars, labels base.Labels,
 			lzVal = base.ValMissing
 			return lzVal
 		}
+	}
+
+	return lzExprFunc
+}
+
+// -----------------------------------------------------
+
+// ExprValsCanonical canonicalizes the vals and joins their bytes
+// together using newlines, similar to base.ValsJoin(). The result is
+// non-JSON / BINARY and can be parsed using base.ValsSplit().
+func ExprValsCanonical(lzVars *base.Vars, labels base.Labels,
+	params []interface{}, path string) (lzExprFunc base.ExprFunc) {
+	var lzBytes []byte // <== varLift: lzBytes by path
+
+	lzExprFunc = func(lzVals base.Vals, lzYieldErr base.YieldErr) (lzVal base.Val) {
+		if LzScope {
+			lzLen := 0
+			for _, lzValX := range lzVals {
+				lzLen += len(lzValX) + 1 // Extra 1 for newline separators.
+			}
+
+			if cap(lzBytes) < lzLen {
+				lzBytes = make([]byte, lzLen*2)
+			}
+
+			var lzErr error
+
+			lzJoined := lzBytes[:0]
+
+			for lzValI, lzValX := range lzVals {
+				if lzValI > 0 {
+					lzJoined = append(lzJoined, '\n')
+				}
+
+				lzJoined, lzErr = lzVars.Ctx.ValComparer.CanonicalJSON(lzValX, lzJoined)
+				if lzErr != nil {
+					break
+				}
+			}
+
+			if lzErr == nil {
+				lzVal = base.Val(lzJoined)
+			}
+
+			lzBytes = lzJoined[:0]
+		}
+
+		return lzVal
 	}
 
 	return lzExprFunc
